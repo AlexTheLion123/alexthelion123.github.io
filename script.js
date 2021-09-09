fb19689e0a7a16527439819439d4af22acd59c61
Alex <alexurban12@gmail.com>
2021-09-09 13:38:34 +0200

    team member click functionality added for small screens, to change big screen layout

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const teamBtn = document.querySelector('.team-popup-wrap .popup .popup-header .btn-close');
 const teamWrapper = document.querySelector('.team-popup-wrap')
 const teamImages = document.querySelectorAll('.container .core-members-wrap .core-members img')
-console.log(teamImages);
+const teamWrapperImage = document.querySelector('.team-popup-wrap .popup .popup-content .popup-top > img');
+const teamWrapperPerson = document.querySelector('.team-popup-wrap .popup .popup-content .popup-top .team-content > p');
+const teamWrapperPosition = document.querySelector('.team-popup-wrap .popup .popup-content .popup-top .team-content > span');
+
 
 // open team-wrapper on image click
 teamImages.forEach(element => {
-    element.addEventListener('click', () => {
+    element.addEventListener('click', e => {
         teamWrapper.style.display = 'block';
+        const image = e.target.currentSrc;
+        const person = element.nextElementSibling.children[0].textContent;
+        const position = element.nextElementSibling.children[1].textContent;        
+
+
+        teamWrapperPerson.textContent = person;
+        teamWrapperPosition.textContent = position;        
+        teamWrapperImage.src = image;
     })
 });
 
 // close team-wrapper on close button click
 teamBtn.addEventListener('click', () => {
-    teamWrapper.style.display = 'none'
+    teamWrapper.style.display = 'none';
 })
 
