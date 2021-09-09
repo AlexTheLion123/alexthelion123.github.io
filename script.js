4630d80bd3289739aa1284dad1a3cebe5abd169a
Alex <alexurban12@gmail.com>
2021-09-09 15:37:53 +0200

    Member page finished

--- script.js
+++ script.js
@@ -5,24 +5,43 @@ const teamWrapperImage = document.querySelector('.team-popup-wrap .popup .popup-
 const teamWrapperPerson = document.querySelector('.team-popup-wrap .popup .popup-content .popup-top .team-content > p');
 const teamWrapperPosition = document.querySelector('.team-popup-wrap .popup .popup-content .popup-top .team-content > span');
 
+let width = window.innerWidth>0 ? window.innerWidth : screen.width;
+if(width<1199) addImageListener();
 
-// open team-wrapper on image click
-teamImages.forEach(element => {
-    element.addEventListener('click', e => {
-        teamWrapper.style.display = 'block';
-        const image = e.target.currentSrc;
-        const person = element.nextElementSibling.children[0].textContent;
-        const position = element.nextElementSibling.children[1].textContent;        
+window.addEventListener('resize', e => {
+    width = window.innerWidth>0 ? window.innerWidth : screen.width;
+    width<=1199 ? addImageListener() : removeImageListener();
+}) 
 
+function addImageListener(){
+    // open team-wrapper on image click
+    teamImages.forEach(element => {
+        person = element.nextElementSibling.children[0].textContent;
+        position = element.nextElementSibling.children[1].textContent;
+        element.addEventListener('click', imageEvent, true)
+    });
 
-        teamWrapperPerson.textContent = person;
-        teamWrapperPosition.textContent = position;        
-        teamWrapperImage.src = image;
+    // close team-wrapper on close button click
+    teamBtn.addEventListener('click', () => {
+        teamWrapper.style.display = 'none';
     })
-});
+}
 
-// close team-wrapper on close button click
-teamBtn.addEventListener('click', () => {
-    teamWrapper.style.display = 'none';
-})
+function removeImageListener(){
+    teamImages.forEach(element => {
+        person = element.nextElementSibling.children[0].textContent;
+        position = element.nextElementSibling.children[1].textContent; 
+        element.removeEventListener('click', imageEvent,true);
+    })
+}
+
+function imageEvent(e){
+    teamWrapper.style.display = 'block';
+    const image = e.target.currentSrc;
+
+    teamWrapperPerson.textContent = person;
+    teamWrapperPosition.textContent = position;        
+    teamWrapperImage.src = image;
+}
+
 
